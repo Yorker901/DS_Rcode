e8d4cc1839cee47c0181d48bdbbb0b73e0fc06ec
Yorker901 <101105495+Yorker901@users.noreply.github.com>
2022-10-16 00:13:37 +0530

    Add files via upload

---
+++ 1.R
@@ -0,0 +1,50 @@
+# create an object
+x = 10
+class(x)
+
+x2 = 10.2
+class(x2)
+
+x3 = "apple"
+x3
+class(x3)
+
+x4 = TRUE
+x4
+class(x4)
+
+x5 = 3.7
+x5
+
+x6 = as.integer(x5)
+x6
+
+x7 = "100"
+x7
+class(x7)
+as.numeric(x7)
+class(x7)
+
+x8 = "monday"
+x8
+class(x8)
+as.numeric(x8)
+
+round(99.9)
+round(99.3)
+as.integer(99.9)
+#===============================================================================
+
+#Arthematic operations on without naming object
+1+3
+10-2
+3*10
+0/1
+1/0
+1/Inf
+10/2
+23%%4 #Gives reminder
+23%/%4 #Returns quotient
+5^2 #Power
+
+abs(1.2)
